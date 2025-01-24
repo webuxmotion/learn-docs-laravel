f329cce4c495dc9ebf2c890cd4558e3c3ac0217b
webuxmotion <pereverziev.andrii@gmail.com>
2025-01-24 23:34:38 +0200

    CSRF Protection

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::match(['get', 'post'], '/', function () {
+    return 'Matched get or post';
+});
+ 
+Route::any('/', function () {
+    return 'Matched any';
+});
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/users', function (Request $request) {
+    dd($request);
+});
+
+
